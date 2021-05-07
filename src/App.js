515c7f018c3fb82b4a37c3842dc0e40832861e5e
Satyaki Bose <satyakibose98@gmail.com>
2021-05-07 20:37:38 +0530

    fix: pin after authentication

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   const [showRegister, setShowRegister] = useState(false);
   const [showLogin, setShowLogin] = useState(false);
   const [viewport, setViewport] = useState({
-    width: "100vw",
+    width: "fit",
     height: "100vh",
     latitude: 22,
     longitude: 88,
@@ -90,6 +90,7 @@ function App() {
         mapStyle="mapbox://styles/satyaki07/cko9n937s4m1e18loytwy0vv6"
         onDblClick={handleAddClick}
         transitionDuration="200"
+        // style={{zIndex:"2"}}
       >
         {pins.map((pin) => {
           return (
@@ -106,6 +107,7 @@ function App() {
                     color:
                       pin.username === currentUser ? "tomato" : "slateblue",
                     cursor: "pointer",
+                    // zIndex:"-1"
                   }}
                   onClick={() => handleMarkerClick(pin._id, pin.lat, pin.long)}
                 />
@@ -136,42 +138,56 @@ function App() {
                   </div>
                 </Popup>
               )}
-              {newPlace && (
-                <Popup
-                  latitude={newPlace.lat}
-                  longitude={newPlace.long}
-                  closeButton={true}
-                  closeOnClick={false}
-                  anchor="left"
-                  onClose={() => setNewPlace(null)}
-                >
-                  <div>
-                    <form onSubmit={handleSubmit}>
-                      <label>Title</label>
-                      <input
-                        placeholder="Enter a title"
-                        onChange={(e) => setTitle(e.target.value)}
-                      />
-                      <label>Review</label>
-                      <textarea
-                        placeholder="Say us something about this place."
-                        onChange={(e) => setDesc(e.target.value)}
-                      />
-                      <label>Rating</label>
-                      <select onChange={(e) => setRating(e.target.value)}>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                      </select>
-                      <button className="submitBtn" type="submit">
-                        Add Pin
-                      </button>
-                    </form>
-                  </div>
-                </Popup>
-              )}
+              {newPlace &&
+                (currentUser ? (
+                  <Popup
+                    latitude={newPlace.lat}
+                    longitude={newPlace.long}
+                    closeButton={true}
+                    closeOnClick={false}
+                    anchor="left"
+                    onClose={() => setNewPlace(null)}
+                  >
+                    <div>
+                      <form onSubmit={handleSubmit}>
+                        <label>Title</label>
+                        <input
+                          placeholder="Enter a title"
+                          onChange={(e) => setTitle(e.target.value)}
+                        />
+                        <label>Review</label>
+                        <textarea
+                          placeholder="Say us something about this place."
+                          onChange={(e) => setDesc(e.target.value)}
+                        />
+                        <label>Rating</label>
+                        <select onChange={(e) => setRating(e.target.value)}>
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                          <option value="3">3</option>
+                          <option value="4">4</option>
+                          <option value="5">5</option>
+                        </select>
+                        <button className="submitBtn" type="submit">
+                          Add Pin
+                        </button>
+                      </form>
+                    </div>
+                  </Popup>
+                ) : (
+                  <Popup
+                    latitude={newPlace.lat}
+                    longitude={newPlace.long}
+                    closeButton={true}
+                    closeOnClick={false}
+                    anchor="left"
+                    onClose={() => setNewPlace(null)}
+                  >
+                    <div>
+                      <h2 style={{color: "slateblue "}}>Please Register / Login</h2>
+                    </div>
+                  </Popup>
+                ))}
             </>
           );
         })}
